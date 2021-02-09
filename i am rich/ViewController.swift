03527c62bd103d6b62e1fcb8e64ab944949516a2
E. Hannah <echannah631@gmail.com>
2021-02-09 14:28:46 -0800

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Elliot Hannah III on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
